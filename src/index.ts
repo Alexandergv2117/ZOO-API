827f7db7df00bcf05ef6131ba16e9215a8b4a80b
Alexander Garcia <alexandergv2117@gmail.com>
2023-02-27 09:04:45 -0500

    Bug: el servidor no corria

--- src/index.ts
+++ src/index.ts
@@ -3,31 +3,6 @@ import cors from 'cors';
 import routerAPI from '@routes/index.routes';
 import * as dotenv from 'dotenv';
 
-import sequelize from './database/index';
-import User from '@models/user';
-import Comment from '@models/comentario';
-import Calificacion from '@models/calificacion';
-import Animal from '@models/animal';
-import Riesgo from '@models/riesgo';
-import TipoReproduccion from '@models/tipo_reproduccion';
-import Alimentacion from '@models/alimentacion';
-import Especie from '@models/especie';
-
-const createdDB = async (): Promise<void> => {
-  await sequelize.sync({ force: true });
-  await Calificacion.findAll();
-  await User.findAll();
-  await Comment.findAll();
-
-  await Riesgo.findAll();
-  await TipoReproduccion.findAll();
-  await Alimentacion.findAll();
-  await Especie.findAll();
-  await Animal.findAll();
-};
-
-void createdDB();
-
 dotenv.config();
 
 const PORT = process.env.PORT;
