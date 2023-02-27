c1c1de2bd6e61d93aea0d910c3c66fca4998277d
Alexander Garcia <alexandergv2117@gmail.com>
2023-02-26 23:56:23 -0500

    tables users, comment and calificacion created

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,24 @@ import cors from 'cors';
 import routerAPI from '@routes/index.routes';
 import * as dotenv from 'dotenv';
 
+import sequelize from './database/index';
+import User from './database/models/user';
+import Comment from './database/models/comentario';
+import Calificacion from './database/models/calificacion';
+
+const createdDB = async (): Promise<void> => {
+  await sequelize.sync({ force: true });
+  await Calificacion.findAll();
+  await User.findAll();
+  await Comment.findAll();
+
+  await User.findAll({
+    include: { model: Comment },
+  });
+};
+
+void createdDB();
+
 dotenv.config();
 
 const PORT = process.env.PORT;
